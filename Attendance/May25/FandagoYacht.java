e9c4670c6cc119991446404630015bdbec01bb2b
Gabriel Marfal <111546382+gabriel00411@users.noreply.github.com>
2023-05-25 15:59:48 +0800

    Update FandagoYacht.java

--- Attendance/May25/FandagoYacht.java
+++ Attendance/May25/FandagoYacht.java
@@ -2,7 +2,7 @@ public class Fandango_Yacht extends Vehicle{
 String MainSailColor = "White";
 	
 void Float() {
-System.out.println("The boat is Sailing");
+System.out.println("The yacht is sailing");
 }
 //Used overriding for method STOP()
 @Override
