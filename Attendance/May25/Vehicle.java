e72cefb0ed04b39c18aeb51c26f623220742e415
Gabriel Marfal <111546382+gabriel00411@users.noreply.github.com>
2023-05-25 16:04:20 +0800

    Create Vehicle.java

---
+++ Attendance/May25/Vehicle.java
@@ -0,0 +1,17 @@
+class Vehicle  {
+String PlaneSpeed = "1000 km/h";
+String PlaneColor = "White";
+String PlanePrice  = "200,000,000 PHP";
+
+String CarSpeed = "80 km/h";
+String CarColor = "Red";
+String CarPrice  = "1,300,000 PHP";
+
+String YachtSpeed = "100 km/h";
+String YachtColor = "Black";
+String YachtPrice  = "20,000,000 PHP";
+
+void Stop () {
+ System.out.println("The vehicle is stopping");	
+}
+}
